d773ccdc6fa5f6368047233e55dcc2bf0a6d5318
Harshavardhan K <harshavardhan816@gmail.com>
2018-10-21 23:27:56 +0530

    Set up locationManager tasks

--- DSCWeatherApp/ViewController.swift
+++ DSCWeatherApp/ViewController.swift
@@ -7,13 +7,51 @@
 //
 
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+
+import SwiftyJSON
+import Alamofire
+
+
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    let APIKEY = "55eba87715d97d1eac62891df01fb00f"
+    let url = "https://api.openweathermap.org/data/2.5/weather"
+    
+    
+    let locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
+        locationManager.requestWhenInUseAuthorization()
     }
+    
+    ///******************************* MARK: - AlamoFire NETWORKING *******************************///
+    ///
+    ///
+    
+    ///******************************* MARK: - Parse JSON  *****************************************///
+    ///
+    ///
+    
+    ///******************************* MARK: - Location Manager Delegate Methods ********************///
+    ///
+    ///
+    
+    ///******************************* MARK: - UI Update *******************************************///
+    ///
+    ///
+    
+    
+    
+    
+    
+    
 
 
 }
