aecc3ac5730f7c7fa58e9dcff9a4608886ea5a2d
Harshavardhan K <harshavardhan816@gmail.com>
2018-10-22 13:50:05 +0530

    Created Mainstoryboard, added fetchWeatherData, updateUI methods

--- DSCWeatherApp/ViewController.swift
+++ DSCWeatherApp/ViewController.swift
@@ -20,6 +20,12 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     let APIKEY = "55eba87715d97d1eac62891df01fb00f"
     let url = "https://api.openweathermap.org/data/2.5/weather"
     
+    var weather = WeatherDataModel()
+    
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var weatherIconLabel: UIImageView!
+    
     
     let locationManager = CLLocationManager()
 
@@ -29,11 +35,33 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.delegate = self
         locationManager.desiredAccuracy = kCLLocationAccuracyKilometer
         locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingHeading()
     }
     
     ///******************************* MARK: - AlamoFire NETWORKING *******************************///
     ///
     ///
+    
+    func getWeatherData(url: String, parameters: Dictionary<String, String>) {
+        
+        Alamofire.request(url, method: .get, parameters: parameters).responseJSON {
+            response in
+            
+            if response.result.isSuccess {
+                print("Success!")
+                
+                
+                let weatherJSON: JSON = JSON(response.result.value!)
+                //print(weatherJSON)
+                
+                self.updateWeatherData(data: weatherJSON)
+                
+            } else {
+                print("Error \(response.result.error)")
+                //self.cityLabel.text = "Connection Issues"
+            }
+        }
+    }
     
     ///******************************* MARK: - Parse JSON  *****************************************///
     ///
@@ -45,14 +73,40 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     
     ///******************************* MARK: - UI Update *******************************************///
     ///
-    ///
     
-    
-    
-    
-    
-    
+    func updateInterfaceWithWeatherData() {
+        
+        cityLabel.text = weather.city
+        tempLabel.text = "\(weather.temperature)"
+        weatherIconLabel.image = UIImage(named: weather.weatherIcon)
+        
+    }
 
+    
+    
+    
+    
+    
+    func updateWeatherData(data: JSON) {
+        
+        if let temperature = data["main"]["temp"].double {
+            
+            weather.temperature = Int(temperature - 273.15)
+            weather.city = data["main"]["city"].stringValue
+            weather.condition = data["weather"][0]["id"].intValue
+            
+            let iconName = weather.updateWeatherIcon(condition: weather.condition)
+            weather.weatherIcon = iconName
+            
+            updateInterfaceWithWeatherData()
+            
+        } else {
+            cityLabel.text = "Weather Unavailable"
+        }
+        
+    }
+    
+    
 
 }
 
